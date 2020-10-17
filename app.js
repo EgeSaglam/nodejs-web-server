b28f7f4de9d0961cc12eb28a28f6e301e6ba137b
Ege Sağlam <egesaglam286@gmail.com>
2020-10-17 12:48:34 +0300

    Changes have made

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-var express = require('express');
-var middleware = require('./middleware');
+var express = require("express");
+var middleware = require("./middleware");
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = 3000;
 
 app.use(middleware.logger);
 
-app.get('/hakkimda', middleware.requireAutentication, function (req, res) {
-    res.send('Hakkimda Sayfası');
-});
+app.get('/', middleware.requireAuthentication, function (req, res) {
+    res.send('Merhabalar');
+})
+
+app.get("/hakkimda", middleware.requireAuthentication, function (req, res) {
+    res.send("Hakkımda sayfası!!!");
+})
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function () {
-    console.log('Express server ' + PORT + 'unda baslatıldı');
-})
+    console.log("Express server " + PORT + " nolu portta çalışıyor...");
+});
